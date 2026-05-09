35689e9df480c7d664f06d9458192de03438bbfb
Peter.Chiu <tidub76217@hotmail.com>
2026-05-09 22:18:12 +0800

    新增測試專案

--- StoryBook/obj/Debug/net10.0/StoryBook.AssemblyInfo.cs
+++ StoryBook/obj/Debug/net10.0/StoryBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoryBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b6bbfd9f222b0da1af5d6732aaeed1cbc96cab1")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoryBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoryBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
